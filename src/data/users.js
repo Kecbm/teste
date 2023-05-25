9a1f7af885db90340944f07284db089d3cfd0c54
Klecianny Melo <kleciannymelo@gmail.com>
2023-05-24 22:22:23 -0300

    :sparkles: arquivo de informações dos usuários

---
+++ src/data/users.js
@@ -0,0 +1,7 @@
+import Avatar from '../assets/Avatar.jpg';
+
+const projects = [
+    { image: Avatar, name: 'Klecianny Melo', bio: 'Tenho 27 anos e moro em Garanhuns, Pernambuco. Sou paixonada por tecnologia e acredito no poder da educação para transformar vidas. Além disso, pratico ciclismo e crossfit para me manter saudável, gosto de ler livros de diversas categorias e jogar videogame. Participo ativamente da Bolha Dev no Twitter, uma comunidade de desenvolvedores que compartilha conhecimentos e experiências sobre o universo tecnológico.', portfolio: 'https://kleciannymelo.vercel.app/', linkedin: 'https://www.linkedin.com/in/kecbm/', github: 'https://codepen.io/klecianny-melo/pen/bGRYLME', twitter: 'https://twitter.com/Kecbm', devto: 'https://dev.to/kecbm', instagram: 'https://www.instagram.com/kecbm/', strava: 'https://www.strava.com/athletes/kecbm', }
+];
+
+export default projects;
