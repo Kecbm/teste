481d2ce731a518ea7bcdcefd7714db239c3d3ffa
Klecianny Melo <kleciannymelo@gmail.com>
2023-07-11 22:36:00 -0300

    :sparkles: informações dos meus irmãos

---
+++ src/utils/array.js
@@ -0,0 +1,14 @@
+const myBrothers = [
+    {
+        name: "Rayanny",
+        emogi: "👩🏾‍🦱",
+        description: "Minha irmã",
+    },
+    {
+        name: "Valdenir",
+        emogi: "🧑‍🦱",
+        description: "Meu irmão"
+    }
+];
+
+console.log("myBrothers: ", myBrothers);
